63adecce5d6e4561a09e414c59178d9867527f64
HoaLe69 <hoabanhanh@gmail.com>
2024-03-25 09:59:58 +0700

    config eslint

--- src/components/shared/button/button.jsx
+++ src/components/shared/button/button.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import style from './button.module.css'
 import classNames from 'classnames/bind'
 const cx = classNames.bind(style)
@@ -5,4 +6,5 @@ const cx = classNames.bind(style)
 const Button = () => {
   return <button className={cx('btn')}>Test</button>
 }
+
 export default Button
