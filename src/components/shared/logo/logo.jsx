9c3372e15b2c92b7757c7863a554ebd3098955c5
XuanHuong2302220 <nickchoigame2302@gmail.com>
2024-03-26 10:45:44 +0700

    fix: logo link

--- src/components/shared/logo/logo.jsx
+++ src/components/shared/logo/logo.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import classNames from 'classnames/bind'
 import styles from './logo.module.css'
 import logoImg from '../../../assets/image/logoImg.svg'
+import { Link } from 'react-router-dom'
 
 const cx = classNames.bind(styles)
 
-const Logo = () => {
+const Logo = ({href}) => {
   return (
-    <div className={cx('container')}>
-      <img src={logoImg} alt="logo" className={cx('image')} />
-      <div className={cx('name')}>
-        <h1>Margelo</h1>
+    <Link to ={href} style={{textDecoration:'none'}}>
+      <div className={cx('container')}>
+        <img src={logoImg} alt="logo" className={cx('image')} />
+        <div className={cx('name')}>
+          <h1>Margelo</h1>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
 export default Logo
+
